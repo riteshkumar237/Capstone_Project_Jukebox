a6a266d194fa9d0bbf8069790682a99dc753438b
ritesh <riteshkumar237@gmail.com>
2022-09-21 09:41:33 +0530

    written query to create a table

--- src/main/resources/sql/songs_queries.sql
+++ src/main/resources/sql/songs_queries.sql
@@ -1,3 +1,14 @@
 -- write query to created a database
 
-create database if not exists jukebox;
+create database if not exists `jukebox`;
+
+-- write query to created table
+
+create table if not exists `jukebox`.`song`
+(
+song_id int not null primary key auto_increment,
+song_name varchar(100) not null,
+artist_name varchar(100) not null,
+duration date not null,
+genre varchar(30)
+);
