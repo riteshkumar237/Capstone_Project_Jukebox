7ce49d59f0d84d59042c57d1f280b31e5389321a
ritesh <riteshkumar237@gmail.com>
2022-09-21 12:25:10 +0530

    created constructor for Playlist

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -5,5 +5,9 @@ public class Playlist extends Song {
     private int playlistId;
     private String playlistName;
 
-
+    public Playlist(int songID, String songName, String artistName, double duration, String genre, int playlistId, String playlistName) {
+        super(songID, songName, artistName, duration, genre);
+        this.playlistId = playlistId;
+        this.playlistName = playlistName;
+    }
 }
