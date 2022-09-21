7a258f26a92a7b8c59ea7666b30b069f99051756
ritesh <riteshkumar237@gmail.com>
2022-09-21 10:28:07 +0530

    created field for Song

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -1,4 +1,10 @@
 package com.niit.jdp.model;
 
 public class Song {
+    private int songID;
+    private String songName;
+    private String artistName;
+    private double duration;
+    private String genre;
+
 }
