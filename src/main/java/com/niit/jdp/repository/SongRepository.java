58e36c18675b8b49658d19832a77ecde876c2444
ritesh <riteshkumar237@gmail.com>
2022-09-21 14:31:17 +0530

    completed getAll() method

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -3,12 +3,36 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Song;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SongRepository {
 
-    public List<Song> getAll(Connection connection) {
+    public List<Song> getAll(Connection connection) throws SQLException {
 
-        return null;
+        String readQuery = "SELECT * FROM `jukebox`.`song`;";
+
+        List<Song> songList = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet songResultSet = statement.executeQuery(readQuery);
+
+            while (songResultSet.next()) {
+                int songId = songResultSet.getInt("song_id");
+                String songName = songResultSet.getString("song_name");
+                String artistName = songResultSet.getString("arist_name");
+                double duration = songResultSet.getDouble("duration");
+                String genre = songResultSet.getString("genre");
+
+                Song song = new Song(songId, songName, artistName, duration, genre);
+
+                songList.add(song);
+            }
+        }
+        return songList;
     }
 }
