bb0c5adc3ea84c7199c9ed01d5e3505956261c8d
ritesh <riteshkumar237@gmail.com>
2022-09-21 11:00:28 +0530

    created constructor and initialized connection

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -1,4 +1,15 @@
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Root@java";
+
+    private Connection connection;
+
+    public DatabaseService() {
+        this.connection = null;
+    }
 }
