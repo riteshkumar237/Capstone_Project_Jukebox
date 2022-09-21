a96188e73c6ec32180889529f2722f912d68ca62
ritesh <riteshkumar237@gmail.com>
2022-09-21 14:10:24 +0530

    created getAll() method

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -1,4 +1,14 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Song;
+
+import java.sql.Connection;
+import java.util.List;
+
 public class SongRepository {
+
+    public List<Song> getAll(Connection connection) {
+
+        return null;
+    }
 }
