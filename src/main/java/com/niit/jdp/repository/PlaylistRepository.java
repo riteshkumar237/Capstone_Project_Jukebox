b5f924a153fcd91e77f2fdd5862b5b7f4f649f89
ritesh <riteshkumar237@gmail.com>
2022-09-21 16:22:07 +0530

    created createPlaylist() method for PlaylistRepository

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -1,4 +1,13 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Song;
+
+import java.sql.Connection;
+
 public class PlaylistRepository {
+
+    public boolean createPlaylist(Connection connection, Song song) {
+
+        return false;
+    }
 }
