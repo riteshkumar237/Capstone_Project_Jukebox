0e341babe67aac0ca8b310f2d507181a30e92a75
ritesh <riteshkumar237@gmail.com>
2022-09-21 10:29:45 +0530

    created no arg constructor and parameterized constructor

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -7,4 +7,14 @@ public class Song {
     private double duration;
     private String genre;
 
+    public Song() {
+    }
+
+    public Song(int songID, String songName, String artistName, double duration, String genre) {
+        this.songID = songID;
+        this.songName = songName;
+        this.artistName = artistName;
+        this.duration = duration;
+        this.genre = genre;
+    }
 }
