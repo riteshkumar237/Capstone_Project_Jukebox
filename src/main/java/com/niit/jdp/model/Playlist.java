bc2a955b519211d4ae12f017c73973647d44c3e6
ritesh <riteshkumar237@gmail.com>
2022-09-21 12:28:57 +0530

    created getter and setter method for Playlist

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -10,4 +10,20 @@ public class Playlist extends Song {
         this.playlistId = playlistId;
         this.playlistName = playlistName;
     }
+
+    public int getPlaylistId() {
+        return playlistId;
+    }
+
+    public void setPlaylistId(int playlistId) {
+        this.playlistId = playlistId;
+    }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
 }
