7ce7f7d03d046adc01f1e70dc5b6d47f2ce42cf6
ritesh <riteshkumar237@gmail.com>
2022-09-21 21:31:31 +0530

    completed displayPlaylist() method

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -2,9 +2,8 @@ package com.niit.jdp.repository;
 
 import com.niit.jdp.model.Playlist;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PlaylistRepository {
@@ -19,15 +18,37 @@ public class PlaylistRepository {
 
             preparedStatement.setInt(1, playlist.getPlaylistId());
             preparedStatement.setString(2, playlist.getPlaylistName());
-            preparedStatement.setInt(3, playlist.getSongID());
+            String songList = playlist.toString().trim().replaceAll("\\[\\]", "");
+            preparedStatement.setString(3, songList);
+
 
             numberOfRowsAffected = preparedStatement.executeUpdate();
         }
         return numberOfRowsAffected > 0;
     }
 
-    public List<Playlist> displayPlaylist(Connection connection) {
+    public List<Playlist> displayPlaylist(Connection connection) throws SQLException {
 
-        return null;
+        String readQuery = "SELECT * FROM `jukebox`.`playlist`;";
+
+        List<Playlist> playlists = new ArrayList<>();
+
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet playlistResultSet = statement.executeQuery(readQuery);
+
+            while (playlistResultSet.next()) {
+
+                int playlistId = playlistResultSet.getInt("playlist_id");
+                String playlistName = playlistResultSet.getString("playlist_name");
+
+
+                Playlist playlist = new Playlist(playlistId, playlistName);
+
+                playlists.add(playlist);
+            }
+        }
+
+        return playlists;
     }
 }
