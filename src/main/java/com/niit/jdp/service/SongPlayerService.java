9da95461c1e9e1a31cd9e8917deafb03c0ba66f9
ritesh <riteshkumar237@gmail.com>
2022-09-22 07:04:53 +0530

    completed player() method

--- src/main/java/com/niit/jdp/service/SongPlayerService.java
+++ src/main/java/com/niit/jdp/service/SongPlayerService.java
@@ -1,8 +1,36 @@
 package com.niit.jdp.service;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class SongPlayerService {
 
-    public void player() {
+    public void player(String songPath) {
+
+        File songFile = new File(songPath);
+
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(songFile);
+
+            Clip clip = AudioSystem.getClip();
+
+            clip.open(audioInputStream);
+
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            clip.start();
+
+            long songDurationInMillisecond = clip.getMicrosecondLength();
+
+            Thread.sleep(songDurationInMillisecond);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException exception) {
+            System.err.println(exception.getMessage());
+            exception.printStackTrace();
+        } catch (InterruptedException exception) {
+            System.err.println("song thread was interrupted");
+        }
 
     }
 
