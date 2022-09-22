10a870ebabf24ece65d62926f0688d97f3aa3845
ritesh <riteshkumar237@gmail.com>
2022-09-22 10:00:20 +0530

    written query to insert value in the table

--- src/main/resources/sql/songs_queries.sql
+++ src/main/resources/sql/songs_queries.sql
@@ -8,6 +8,7 @@ create table if not exists `jukebox`.`song`
 (
 song_id int not null primary key auto_increment,
 song_name varchar(100) not null,
+song_path varchar(1000) not null,
 artist_name varchar(100) not null,
 duration double not null,
 genre varchar(30) not null
@@ -15,4 +16,13 @@ genre varchar(30) not null
 
 -- write query to insert value in the table.
 
-INSERT INTO `jukebox`.`song` (`song_id`, `song_name`, `artist_name`, `duration`, `genre`) VALUES ('1', 'Taare Zameen Par', 'Shankar Mahadevan', '7.13', 'Drama');
+INSERT INTO `jukebox`.`song` (`song_id`, `song_name`, `artist_name`, `duration`, `genre`) VALUES ('1', 'Taare Zameen Par', 'C:\\Users\\rites\\IdeaProjects\\Capstone_Project_Jukebox\\src\\main\\resources\\song\\Taare Zameen Par.wav','Shankar Mahadevan', '7.13', 'Drama');
+
+INSERT INTO `jukebox`.`song` (`song_id`, `song_name`, `song_path`, `artist_name`, `duration`) VALUES ('2', 'Bheed Mein Tanhai Mein', 'C:\\Users\\rites\\IdeaProjects\\Capstone_Project_Jukebox\\src\\main\\resources\\song\\Bheed Mein Tanhai Mein.wav', 'Udit Narayan', '6.00', 'Sad');
+
+INSERT INTO `jukebox`.`song` (`song_name`, `song_path`, `artist_name`, `duration`, `genre`) VALUES ('Hum Tumko Nigahon Mein', 'C:\\Users\\rites\\IdeaProjects\\Capstone_Project_Jukebox\\src\\main\\resources\\song\\Hum Tumko Nigahon Mein.wav', 'Udit Narayan', '6.03', 'Pop');
+
+INSERT INTO `jukebox`.`song` (`song_name`, `song_path`, `artist_name`, `duration`, `genre`) VALUES ('Rok Sako To Rok Lo', 'C:\\Users\\rites\\IdeaProjects\\Capstone_Project_Jukebox\\src\\main\\resources\\song\\Rok Sako To Rok Lo.wav', 'Shaan', '4.42', 'Comedy');
+
+INSERT INTO `jukebox`.`song` (`song_name`, `song_path`, `artist_name`, `duration`, `genre`) VALUES ('Sapna Koi', 'C:\\Users\\rites\\IdeaProjects\\Capstone_Project_Jukebox\\src\\main\\resources\\song\\Sapna Koi.wav', 'Asha Bhosle', '3.39', 'Drama');
+
