6a24d24cb619e8e24a04316d730a240a6968da7c
ritesh <riteshkumar237@gmail.com>
2022-09-23 12:21:34 +0530

    changed line 32 and 46

--- src/main/java/com/niit/jdp/service/SongPlayerService.java
+++ src/main/java/com/niit/jdp/service/SongPlayerService.java
@@ -1,30 +1,13 @@
 package com.niit.jdp.service;
 
 import com.niit.jdp.Main;
-import com.niit.jdp.model.Playlist;
-import com.niit.jdp.model.Song;
 
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 
 public class SongPlayerService {
-
-    int songIndex;
-
-    public void play(List<Song> songsList) {
-
-        Playlist playlist = new Playlist();
-
-        for (int i = 0; i < playlist.getSongList().size(); i++) {
-            songIndex = i;
-            player(songsList.get(i).getSongPath());
-        }
-
-    }
-
 
     public void player(String songPath) {
 
@@ -43,29 +26,24 @@ public class SongPlayerService {
             String response = "";
 
             while (!response.equals("Q")) {
-                System.out.println("p = play, t= Pause, s=Stop, r = Reset, q = Quit, e = Exit, m = MAIN MENU");
+                System.out.println("p = play, t= Pause, s=Stop, q = Quit, m = MAIN MENU");
                 System.out.print("Enter your choice: ");
 
                 response = scanner.next();
 
 
                 switch (response) {
-                    case ("p"): {
+                    case ("p"):
                         clip.start();
                         break;
-                    }
-                    case ("t"): {
-                        clip.stop();
-                        break;
-                    }
-                    case ("s"): {
-                        clip.setMicrosecondPosition(0);
-                        clip.stop();
-                        break;
-                    }
 
-                    case ("r"):
+                    case ("t"):
+                        clip.stop();
+                        break;
+
+                    case ("s"):
                         clip.setMicrosecondPosition(0);
+                        clip.stop();
                         break;
 
                     case ("q"):
@@ -75,9 +53,6 @@ public class SongPlayerService {
                     case ("m"):
                         String[] arg = new String[0];
                         Main.main(arg);
-                        break;
-                    case ("e"):
-                        System.exit(0);
                         break;
 
                     default:
