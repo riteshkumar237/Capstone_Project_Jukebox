3a6a7e7b229b67744955cbcf29fa44bfac27501b
ritesh <riteshkumar237@gmail.com>
2022-09-25 23:25:16 +0530

    written test case for database connection

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,6 +1,29 @@
 package com.niit.jdp.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class DatabaseServiceTest {
 
+    DatabaseService databaseService;
+
+    @BeforeEach
+    void setUp() {
+        databaseService = new DatabaseService();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void databaseConnected() throws SQLException {
+        assertTrue(databaseService.connect());
+    }
 
 }
